733fa7a558c323260257bfb0783a22c70b486449
D. Klevinghaus <adk@codekommando.de>
2019-01-01 22:19:03 +0100

    Updates for travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 module.exports = function(config) {
-  config.set({
+  let configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -55,10 +55,23 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
+    
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+    configuration.singleRun = true;
+  }
+
+  config.set(configuration);
 }
